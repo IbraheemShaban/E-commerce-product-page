118dd7a2075d76acd485ef9a3a6548bb19affd08
IbraheemShaban <ishaban097@gmail.com>
2022-04-22 05:41:13 +0300

    FINISHED the task

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let orderCount = 0;
 let cartIsOpen = false;
-// let deleteIsClicked = false;
-document.getElementById('cart-open-screen').classList.add('hide-div');
-document.getElementById('cart-is-empty').classList.add('hide-div');
-document.getElementById('number-on-cart-icon').classList.add('hide-div');
+showOrHide('hide', 'cart-open-screen');
+showOrHide('hide', 'cart-is-empty');
+showOrHide('hide', 'number-on-cart-icon');
+showOrHide('hide', 'lightbox');
+showOrHide('hide', 'lightbox2');
 let addedItemsCount = 0;
+
 // + - functuality
 document.getElementById('orderCount').innerHTML = orderCount;
 document.getElementById('plus').onclick = function () {
@@ -17,50 +19,137 @@ document.getElementById('minus').onclick = function () {
     document.getElementById('orderCount').innerHTML = orderCount;
   }
 };
+
 // adding items then opeining cart functuality
 document.getElementById('add-button').onclick = function () {
   addedItemsCount += orderCount;
   if (orderCount > 0) {
-    document.getElementById('cart-is-empty').classList.add('hide-div');
-    document.getElementById('cart-has-items').classList.remove('hide-div');
+    showOrHide('hide', 'cart-is-empty');
+    showOrHide('show', 'cart-has-items');
+
     document.getElementById(
       'items-bought-description-summary'
-    ).innerHTML = `Fall Limited Edition Sneakers $125 x ${addedItemsCount} <b>${
+    ).innerHTML = `Fall Limited Edition Sneakers $125 x ${addedItemsCount} <b style = "color: black">$${
       addedItemsCount * 125
-    }<b>`;
-    document.getElementById('number-on-cart').innerHTML = addedItemsCount;
-    document.getElementById('cart-open-screen').classList.remove('hide-div');
-    document.getElementById('number-on-cart-icon').classList.remove('hide-div');
+    }.00<b>`;
 
+    document.getElementById('number-on-cart').innerHTML = addedItemsCount;
+    showOrHide('show', 'cart-open-screen');
+    showOrHide('show', 'number-on-cart-icon');
     cartIsOpen = true;
   }
 };
 // opening and closing cart when cart button pressed
 document.getElementById('cart').onclick = function () {
   if (addedItemsCount == 0) {
-    document.getElementById('cart-has-items').classList.add('hide-div');
-    document.getElementById('number-on-cart-icon').classList.add('hide-div');
-
-    document.getElementById('cart-is-empty').classList.remove('hide-div');
+    showOrHide('hide', 'cart-has-items');
+    showOrHide('hide', 'number-on-cart-icon');
+    showOrHide('show', 'cart-is-empty');
   }
   if (cartIsOpen) {
-    document.getElementById('cart-open-screen').classList.add('hide-div');
+    showOrHide('hide', 'cart-open-screen');
     cartIsOpen = false;
   } else if (!cartIsOpen) {
-    document.getElementById('cart-open-screen').classList.remove('hide-div');
-    // document.getElementById('number-on-cart-icon').classList.remove('hide-div');
+    showOrHide('show', 'cart-open-screen');
     cartIsOpen = true;
   }
 };
 // delete button functuality
 document.getElementById('delete').onclick = function () {
   addedItemsCount = 0;
-  //   deleteIsClicked = true;
-  document.getElementById('items-bought-description-summary').innerHTML = '';
-  document.getElementById('number-on-cart-icon').innerHTML = '';
+  showOrHide('hide', 'cart-has-items');
+  showOrHide('hide', 'number-on-cart-icon');
+  showOrHide('show', 'cart-is-empty');
+};
 
-  document.getElementById('cart-has-items').classList.add('hide-div');
-  document.getElementById('number-on-cart-icon').classList.add('hide-div');
-
-  document.getElementById('cart-is-empty').classList.remove('hide-div');
+function showOrHide(state, div) {
+  if (state.toLowerCase() === 'show') {
+    document.getElementById(div).classList.remove('hide-div');
+  }
+  if (state.toLowerCase() === 'hide') {
+    document.getElementById(div).classList.add('hide-div');
+  }
+}
+let order = 1;
+document.getElementById('thumbnail1').onclick = function () {
+  document.getElementById('big-image').src = '/images/image-product-1.jpg';
+};
+document.getElementById('thumbnail2').onclick = function () {
+  document.getElementById('big-image').src = '/images/image-product-2.jpg';
+};
+document.getElementById('thumbnail3').onclick = function () {
+  document.getElementById('big-image').src = '/images/image-product-3.jpg';
+};
+document.getElementById('thumbnail4').onclick = function () {
+  document.getElementById('big-image').src = '/images/image-product-4.jpg';
+};
+///////
+document.getElementById('lightbox-thumbnail1').onclick = function () {
+  document.getElementById('gallery-big-image').src =
+    '/images/image-product-1.jpg';
+  document.getElementById('gallery-big-image2').src =
+    '/images/image-product-1.jpg';
+  order = 1;
+};
+document.getElementById('lightbox-thumbnail2').onclick = function () {
+  document.getElementById('gallery-big-image').src =
+    '/images/image-product-2.jpg';
+  document.getElementById('gallery-big-image2').src =
+    '/images/image-product-2.jpg';
+  order = 2;
+};
+document.getElementById('lightbox-thumbnail3').onclick = function () {
+  document.getElementById('gallery-big-image').src =
+    '/images/image-product-3.jpg';
+  document.getElementById('gallery-big-image2').src =
+    '/images/image-product-3.jpg';
+  order = 3;
+};
+document.getElementById('lightbox-thumbnail4').onclick = function () {
+  document.getElementById('gallery-big-image').src =
+    '/images/image-product-4.jpg';
+  document.getElementById('gallery-big-image2').src =
+    '/images/image-product-4.jpg';
+  order = 4;
+};
+document.getElementById('next').onclick = function () {
+  if (order != 4) {
+    order++;
+    document.getElementById(
+      'gallery-big-image'
+    ).src = `/images/image-product-${order}.jpg`;
+    document.getElementById(
+      'gallery-big-image2'
+    ).src = `/images/image-product-${order}.jpg`;
+  }
+};
+document.getElementById('previous').onclick = function () {
+  if (order != 1) {
+    order--;
+    document.getElementById(
+      'gallery-big-image'
+    ).src = `/images/image-product-${order}.jpg`;
+    document.getElementById(
+      'gallery-big-image2'
+    ).src = `/images/image-product-${order}.jpg`;
+  }
+};
+document.getElementById('close').onclick = function () {
+  showOrHide('hide', 'lightbox');
+  showOrHide('hide', 'lightbox2');
+};
+document.getElementById('big-image').onclick = function () {
+  showOrHide('show', 'lightbox');
+  showOrHide('show', 'lightbox2');
+  document.getElementById('gallery-big-image').src =
+    document.getElementById('big-image').src;
+  document.getElementById('gallery-big-image2').src =
+    document.getElementById('big-image').src;
+  order = parseInt(
+    document.getElementById('big-image').src[
+      document.getElementById('big-image').src.length - 5
+    ]
+  );
+  console.log(order);
+  console.log(document.getElementById('big-image').src);
 };
